a936f17f98bb7596311cfcbd4436e2c6ceed4413
Andrew Kelly <drew12396@gmail.com>
2018-10-13 15:40:16 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Andrew Kelly on 10/13/18.
+//  Copyright © 2018 Andrew Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
