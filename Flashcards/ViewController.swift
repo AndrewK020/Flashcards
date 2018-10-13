8bec6b897d29670b50a07b6a325467fc41300377
Andrew Kelly <drew12396@gmail.com>
2018-10-13 16:20:53 -0500

    simple flashcard lab done

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func didTapOnFlashCard(_ sender: Any) {
+        if (answerLabel.isHidden == true){
+            answerLabel.isHidden = false
+        }
+        else if (answerLabel.isHidden == false){
+            answerLabel.isHidden = true
+        }
+    }
+    
 }
 
